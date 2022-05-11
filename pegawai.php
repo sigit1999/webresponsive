cf3e8012c6a0cb82cf3ec6f8fed3001b2c363a47
sigit1999 <104711699+sigit1999@users.noreply.github.com>
2022-05-11 19:21:21 +0700

    hapus pegawai

--- pegawai.php
+++ pegawai.php
@@ -163,7 +163,7 @@ require 'ceklog.php'
                                                 <div class="modal-dialog">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Hapus KB</h5>
+                                                            <h5 class="modal-title" id="exampleModalLabel">Hapus Pegawai</h5>
                                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                             </button>
